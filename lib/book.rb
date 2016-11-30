ea15f4fdcd081748637c8d4e9b1f17d67ba0ea3e
github username <github email address>
2016-11-30 11:42:35 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre, :turn_page
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title=(title)
+    @title = title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+book = Book.new("And Then There Were None")
+book.page_count = 272
+book.genre ="Mystery"
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
-# Make your shoe class here!
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand=(brand)
+    @brand = brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
+shoe = Shoe.new("Adidas")
+
